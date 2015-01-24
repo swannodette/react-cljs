2b6f3ec17c7466ab4db033cc6650616424a4bbc9
David Nolen <david.nolen@gmail.com>
2015-01-24 08:38:32 -0500

    0.12.2.4

--- src/deps.cljs
+++ src/deps.cljs
@@ -1,6 +1,9 @@
 {
  :foreign-libs [{:file     "react/react.js"
                  :file-min "react/react.min.js"
-                 :provides ["com.facebook.React"]}]
+                 :provides ["com.facebook.React"]}
+                {:file     "react/react_with_addons.js"
+                 :file-min "react/react_with_addons.min.js"
+                 :provides ["com.facebook.ReactWithAddons"]}]
  :externs ["react/externs/react.js"]
 }
